1d44e19b8ea44e8d0ba1c3d02b1c907e81ba9434
Anthony Anjorin <anthony.anjorin@uni-paderborn.de>
2015-11-06 15:29:52 +0000

    added action to convert .tgg.xmi to mosl tgg

--- org.moflon.tgg.mosl/src/org/moflon/tgg/mosl/builder/MOSLTGGConversionHelper.java
+++ org.moflon.tgg.mosl/src/org/moflon/tgg/mosl/builder/MOSLTGGConversionHelper.java
@@ -1,13 +1,19 @@
 package org.moflon.tgg.mosl.builder;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.xtext.resource.XtextResourceSet;
 
-public class MOSLTGGConversionHelper {
+public class MOSLTGGConversionHelper extends AbstractHandler {
 
 	public static void generateTGGModel(IResource resource) {
 		
@@ -31,5 +37,23 @@ public class MOSLTGGConversionHelper {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+
+		if (selection instanceof IStructuredSelection) {
+			Object file = ((IStructuredSelection) selection).getFirstElement();
+			if(file instanceof IFile){
+				IFile tggFile = (IFile)file;
+				
+				//TODO Invoke TGG backward transformation to get the mosl tgg model
+				
+				//TODO Persist mosl tgg model as set of xtext files
+			}
+		}
+
+		return null;
 	}
 }
