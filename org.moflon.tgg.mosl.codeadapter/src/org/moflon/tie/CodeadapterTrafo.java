1634e2f51a2a7c1315de5846b06e6086be3c3272
Thomas Kovac <thomas.kovac@t-online.de>
2016-02-02 10:21:04 +0000

    removed unused imports

--- org.moflon.tgg.mosl.codeadapter/src/org/moflon/tie/CodeadapterTrafo.java
+++ org.moflon.tgg.mosl.codeadapter/src/org/moflon/tie/CodeadapterTrafo.java
@@ -1,55 +1,11 @@
 package org.moflon.tie;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.log4j.BasicConfigurator;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.emf.common.util.BasicEList;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.moflon.tgg.algorithm.synchronization.SynchronizationHelper;
-import org.moflon.tgg.language.Domain;
-import org.moflon.tgg.language.DomainType;
-import org.moflon.tgg.language.TGGLinkVariable;
-import org.moflon.tgg.language.TripleGraphGrammar;
-import org.moflon.tgg.language.csp.AttributeVariable;
-import org.moflon.tgg.language.csp.Literal;
-import org.moflon.tgg.language.csp.LocalVariable;
-import org.moflon.tgg.language.csp.Variable;
-import org.moflon.tgg.mosl.codeadapter.AttrCondToTGGConstraint;
-import org.moflon.tgg.mosl.codeadapter.AttributeAssignmentToAttributeAssignment;
-import org.moflon.tgg.mosl.codeadapter.AttributeConstraintToConstraint;
 import org.moflon.tgg.mosl.codeadapter.CodeadapterPackage;
-import org.moflon.tgg.mosl.codeadapter.CorrTypeToEClass;
-import org.moflon.tgg.mosl.codeadapter.CorrVariablePatternToTGGObjectVariable;
-import org.moflon.tgg.mosl.codeadapter.ExpressionToExpression;
-import org.moflon.tgg.mosl.codeadapter.LinkVariablePatternToTGGLinkVariable;
-import org.moflon.tgg.mosl.codeadapter.ObjectVariablePatternToTGGObjectVariable;
-import org.moflon.tgg.mosl.codeadapter.TripleGraphGrammarFileToTripleGraphGrammar;
-import org.moflon.tgg.mosl.tgg.AttributeExpression;
-import org.moflon.tgg.mosl.tgg.CorrType;
-import org.moflon.tgg.mosl.tgg.CorrVariablePattern;
-import org.moflon.tgg.mosl.tgg.Import;
-import org.moflon.tgg.mosl.tgg.ParamValue;
-import org.moflon.tgg.mosl.tgg.Rule;
-import org.moflon.tgg.mosl.tgg.Schema;
-import org.moflon.tgg.mosl.tgg.TggFactory;
-import org.moflon.tgg.mosl.tgg.TripleGraphGrammarFile;
-import org.moflon.tgg.mosl.tgg.Using;
-import org.moflon.tgg.runtime.RuntimePackage;
-import org.moflon.tgg.tggproject.TGGProject;
-
-import SDMLanguage.expressions.ComparisonExpression;
-import SDMLanguage.patterns.LinkVariable;
-import SDMLanguage.patterns.patternExpressions.AttributeValueExpression;
 
 
 public class CodeadapterTrafo extends SynchronizationHelper{
