c652ce2808fd2915ddcda1e7bfecf792d81f1048
Anthony Anjorin <anthony.anjorin@uni-paderborn.de>
2016-02-03 21:53:35 +0000

    Slightly adjusted the default style of keywords (not bold, only italics)

--- org.moflon.tgg.mosl.ui/src/org/moflon/tgg/mosl/ui/TGGUiModule.java
+++ org.moflon.tgg.mosl.ui/src/org/moflon/tgg/mosl/ui/TGGUiModule.java
@@ -4,6 +4,8 @@
 package org.moflon.tgg.mosl.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
+import org.moflon.tgg.mosl.ui.highlighting.MOSLHighlightingConfiguration;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -11,5 +13,10 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class TGGUiModule extends org.moflon.tgg.mosl.ui.AbstractTGGUiModule {
 	public TGGUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+	
+	public Class<? extends IHighlightingConfiguration> bindIHighlightingConfiguration()
+	{
+	   return MOSLHighlightingConfiguration.class;
 	}
 }
