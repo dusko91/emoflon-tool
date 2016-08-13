b40b78e5793350ab1104c0106c16e17c3a72cad8
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2016-08-13 07:48:43 +0000

    Clean up

--- gervarro/org.moflon.core.utilities/src/org/moflon/core/utilities/eMoflonEMFUtil.java
+++ gervarro/org.moflon.core.utilities/src/org/moflon/core/utilities/eMoflonEMFUtil.java
@@ -3,7 +3,6 @@ package org.moflon.core.utilities;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
