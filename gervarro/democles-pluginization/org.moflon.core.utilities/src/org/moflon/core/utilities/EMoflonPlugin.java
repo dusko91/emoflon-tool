112ff5048f5bf328f0b402372552b750966af8e8
Gergely Varró <gergely.varro@es.tu-darmstadt.de>
2016-09-13 09:55:27 +0000

    Merging latest changes in the trunk to the Democles pluginization development branch

--- gervarro/democles-pluginization/org.moflon.core.utilities/src/org/moflon/core/utilities/EMoflonPlugin.java
+++ gervarro/democles-pluginization/org.moflon.core.utilities/src/org/moflon/core/utilities/EMoflonPlugin.java
@@ -3,8 +3,11 @@ package org.moflon.core.utilities;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * This is a generic base class for plugins in EMoflon.
@@ -12,7 +15,12 @@ import org.osgi.framework.BundleContext;
  * Its {@link #pluginId} attribute is initialized during
  * {@link #start(BundleContext)} and de-initialized during
  * {@link #stop(BundleContext)}.
+ * 
+ * @deprecated This class was basically introduced to easily and consistently access plugin IDs. 
+ * In fact, this features is already built into Eclipse using {@link FrameworkUtil#getBundle(Class)}
+ * Example: new Status(IStatus.OK, FrameworkUtil.getBundle(getClass()).getSymbolicName(), "My message.");
  */
+@Deprecated
 public abstract class EMoflonPlugin extends Plugin {
 	private String pluginId;
 	private static Map<Class<?>, EMoflonPlugin> typeToPluginMap = new HashMap<>();
